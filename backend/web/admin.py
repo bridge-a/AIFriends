8f30700fa27e49bfd17f12f24ef0a678ddaaa27d
bridge-a <1365220401@qq.com>
2026-07-17 23:37:49 +0800

    accomplish friend page backend

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,8 +1,12 @@
+from atexit import register
+
 from django.contrib import admin
 
 # Register your models here.
 from web.models.user import UserProfile
 from web.models.character import Character
+from web.models.friend import Friend
+
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
@@ -11,4 +15,8 @@ class UserProfileAdmin(admin.ModelAdmin):
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
+
+@admin.register(Friend)
+class FriendAdmin(admin.ModelAdmin):
+    raw_id_fields = ('me', 'character',)
 
