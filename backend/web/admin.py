fa8cfe0e4ac239fafbbd7dc23b98708d5de25b7c
bridge-a <1365220401@qq.com>
2026-07-19 01:58:21 +0800

    create message database

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,22 +1,24 @@
-from atexit import register
-
 from django.contrib import admin
-
-# Register your models here.
 from web.models.user import UserProfile
 from web.models.character import Character
-from web.models.friend import Friend
+from web.models.friend import Friend, Message
 
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    raw_id_fields = ('user',) #逗号千万不要删！！！！
+    raw_id_fields = ('user',)  #逗号千万不要删！！！！
+
 
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
 
+
 @admin.register(Friend)
 class FriendAdmin(admin.ModelAdmin):
     raw_id_fields = ('me', 'character',)
 
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    raw_id_fields = ('friend',)
